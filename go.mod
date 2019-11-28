016f12312afcfb7e8153a75a93484e13c06536d4
Sergey <xxsmotur@gmail.com>
2019-11-28 22:36:03 +0500

    Add zookeeper locks

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/saromanov/locker
 
 go 1.13
 
-require github.com/go-redis/redis v6.15.6+incompatible // indirect
+require (
+	github.com/go-redis/redis v6.15.6+incompatible // indirect
+	github.com/samuel/go-zookeeper v0.0.0-20190923202752-2cc03de413da
+)
--- pkg/lock/zk/zk.go
+++ pkg/lock/zk/zk.go
@@ -1,1 +1,40 @@
 package zk
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/samuel/go-zookeeper/zk"
+	"github.com/saromanov/locker/pkg/lock"
+)
+
+type zkLock struct {
+	client *zk.Conn
+}
+
+func New() lock.Locker {
+	c, _, err := zk.Connect([]string{"127.0.0.1"}, time.Second)
+	if err != nil {
+		fmt.Println("unable to connect to ZooKeeper")
+		return nil
+	}
+	return &zkLock{
+		client: c,
+	}
+}
+
+func (t *zkLock) Lock() bool {
+	l := zk.NewLock(t.client, "/lock", zk.WorldACL(zk.PermAll))
+	err := l.Lock()
+	if err != nil {
+		fmt.Println("unable to make Lock: %v", err)
+		return false
+	}
+
+	return true
+}
+
+func (t *zkLock) Unlock() {
+	l := zk.NewLock(t.client, "/lock", zk.WorldACL(zk.PermAll))
+	l.Unlock()
+}
