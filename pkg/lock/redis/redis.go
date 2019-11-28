44abe6bf473a5d8c8299ced206bf0471ea40b3b4
Sergey <xxsmotur@gmail.com>
2019-11-28 22:27:42 +0500

    Add more methods for locking on redis

--- pkg/lock/redis/redis.go
+++ pkg/lock/redis/redis.go
@@ -1,12 +1,19 @@
 package redis
 
 import (
+	"fmt"
+	"time"
 	"github.com/go-redis/redis"
 	"github.com/saromanov/locker/pkg/lock"
 )
 
+const (
+	lockerKey = "locker"
+)
+
 // redisLock provides implementation of locks from redis
 type redisLock struct {
+	client *redis.Client
 }
 
 func New() *lock.Locker {
@@ -15,5 +22,32 @@ func New() *lock.Locker {
 		Password: "",
 		DB:       0,
 	})
-	return &redisLock{}
+	return &redisLock{
+		client: client,
+	}
+}
+
+func (t *redisLock) Lock() bool {
+	resp := t.client.SetNX(lockerKey, 1, time.Second*5)
+	lockSuccess, err := resp.Result()
+	if err != nil {
+		fmt.Println("unable to apply lock: ", err)
+		return false
+	}
+	if !lockSuccess {
+		fmt.Println("unable to apply lock")
+		return false
+	}
+
+	return true
+}
+
+func (t *redisLock) Unlock() {
+	delResp := t.client.Del(lockerKey)
+	unlockSuccess, err := delResp.Result()
+	if err == nil && unlockSuccess > 0 {
+		println("unlock success!")
+	} else {
+		println("unlock failed", err)
+	}
 }
