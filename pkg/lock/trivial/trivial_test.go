c8f3bfc55557bbdee440ee6afed7500e3082bb06
Sergey <xxsmotur@gmail.com>
2019-11-29 13:04:11 +0500

    Additional test

--- pkg/lock/trivial/trivial_test.go
+++ pkg/lock/trivial/trivial_test.go
@@ -1,10 +1,30 @@
 package trivial
 
 import (
+	"sync"
 	"testing"
+
 	"github.com/stretchr/testify/assert"
 )
+
 func TestLock(t *testing.T) {
 	l := New()
 	assert.Equal(t, l.Lock(), true)
+}
+
+func TestLocks(t *testing.T) {
+	var l = New()
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if !l.Lock() {
+				assert.Fail(t, "lock is failed")
+				return
+			}
+			l.Unlock()
+		}()
+	}
+	wg.Wait()
 }
